0ac51991f95ba6948bb6ae2c9cd9542674e226ab
David Sepulveda <david.sepulvedamarti@ucalgary.ca>
2019-03-18 09:30:53 -0600

    Done matrix rain

--- scripts/webScripts.js
+++ scripts/webScripts.js
@@ -1,168 +1,101 @@
-var canvas = document.querySelector('.myCanvas');
-var width = canvas.width = window.innerWidth;
-var height = canvas.height = window.innerHeight;
-var ctx = canvas.getContext('2d');
+/*
+ * Based heavily off of https://codepen.io/doms/pen/BZLezr
+ * I had originally given up on the idea because I figured with the implementation
+ * that I had in mind, it would be too slow to implement. Upon seeing Dominic's
+ * implementation running smoothly, however, I decided I'd give it a shot. I 
+ * had essentially the same implementation in mind so I was excited to find out
+ * that it ran more or less quickly and efficiently. I did not want to use p5.js though,
+ * so I ended up trying to replicate his code without the use of p5.js. This resulted
+ * in what seems to be a noticably slower implementation. While unfortunate that I 
+ * still ended up in the same place as when I started, I've put it in enough time and
+ * effort into this that I feel the user can just disable javascript for my page if it
+ * bothers them.
+ */
 
-var r = 0;
-var g = 255;
-var b = 0;
-
-//ctx.beginPath();
-ctx.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
-var orix = -1;
-var oriy = -1;
-
-var points = [];
-var oldPoints = []
-lastPoint = 0;
-
-var colorChange = -1;
-var number = -1;
-
-document.body.onkeyup = function(e){
-    if(e.keyCode == 32){
-
-    	ctx.beginPath();
-
-    	for (var i = 0; i < points.length; i += 2){
-    		ctx.lineTo(points[i], points[i+1]);
-    	}
-    	ctx.lineTo(points[0], points[1]);
-    	ctx.fill();
-
-
-      oldPoints = points;
-    	points = [];
-    	lastPoint = 0;
-
-    	orix = -1;
-    	oriy = -1;
+class Particle {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.speed = 1;
+        this.width = 30;
+        this.font = this.width + "px monospace"
+        this.residue = [];
+        this.timer = 0;
+        this.reset();
     }
 
-    else if (e.key == 'x'){
-      var origColor = ctx.fillStyle;
+    reset() {
+        this.x = Math.random() * innerWidth;
+        this.y = (Math.random() * 10) + 10;
+        this.width = (Math.random() * 20) + 10;
+        this.font = this.width + "px monospace"
+        this.timer = 0;
+
+        this.speed = (Math.random() * 1.2) + 1.0;
+    }
 
 
-      for (var j = 0; j < random(3, 20); j++){
-        var yOff = random(-50, 50);
-        var xOff = random(-50, 50);
-        
-        ctx.beginPath();
-        for (var i = 0; i < oldPoints.length; i += 2){
-          ctx.lineTo(oldPoints[i] + xOff, oldPoints[i+1] + yOff);
+    draw() {
+
+        var gradient = ctx.createLinearGradient(this.x, this.y - (this.width * 4), this.x, this.y);
+        gradient.addColorStop("0", "black");
+        gradient.addColorStop("1.0", "lime");
+        ctx.font = this.font;
+        ctx.fillStyle = gradient;
+        if (this.timer == 0){
+            for (let k = 0; k < 4; k++){
+                let char = String.fromCharCode(Math.floor((Math.random() * 109) + 33));
+                this.residue.push(char);
+            }
+            for (let k = 0; k < 3; k++){
+                this.residue.shift();
+            }
+            if (this.residue.length > 4){
+                this.residue.length = 4;
+            }
         }
-        ctx.lineTo(oldPoints[0] + xOff, oldPoints[1] + yOff);
 
-        var newR = r + random(-120, 120);
-        var newG = g + random(-120, 120);
-        var newB = b + random(-120, 120);
+        for (let j = 0; j < this.residue.length; j++){
+            if ((this.residue.length - 1 - j) >= 0){
+                ctx.fillText(this.residue[this.residue.length - 1 - j], this.x, this.y - (this.width * j));
+            }
+        }
 
+        this.timer++;
 
-        ctx.fillStyle = 'rgb(' + (r + random(-10, 10)) + ', ' + (g + random(-10, 10)) + ', ' + (b + random(-10, 10)) + ')';
+        if ((this.speed * this.timer) >= (this.width/15)){
+            this.timer = 0;
+        }
+        if (this.y > (innerHeight + this.width)) {
+            this.reset();
+        }
 
-        ctx.fillStyle = 'rgb(' + newR + ', ' + newG + ', ' + newB + ')';
-
-        ctx.fill();
-      }
-      ctx.fillStyle = origColor;
-
-    }
-
-    else if (e.key == 'r'){
-    	number = 0;
-    	colorChange = 0;
-    }
-    else if (e.key == 'g'){
-    	number = 0;
-    	colorChange = 1;
-    }
-    else if (e.key == 'b'){
-    	number = 0;
-    	colorChange = 2;
-    }
-
-
-
-    else if (e.key == '0'){
-    	number *= 10;
-    }
-    else if (e.key == '1'){
-    	number *= 10;
-    	number += 1;
-    }
-    else if (e.key == '2'){
-    	number *= 10;
-    	number += 2;
-    }
-    else if (e.key == '3'){
-    	number *= 10;
-    	number += 3;
-    }
-    else if (e.key == '4'){
-    	number *= 10;
-    	number += 4;
-    }
-    else if (e.key == '5'){
-    	number *= 10;
-    	number += 5;
-    }
-    else if (e.key == '6'){
-    	number *= 10;
-    	number += 6;
-    }
-    else if (e.key == '7'){
-    	number *= 10;
-    	number += 7;
-    }
-    else if (e.key == '8'){
-    	number *= 10;
-    	number += 8;
-    }
-    else if (e.key == '9'){
-    	number *= 10;
-    	number += 9;
-    }
-    else if (e.keyCode = '13'){
-    	if (!(colorChange == -1)){
-    		if ((number >= 0) && (number <= 255)){
-	    		if (colorChange == 0){
-	    			r = number;
-	    		}
-	    		else if (colorChange == 1){
-	    			g = number;
-	    		}
-	    		else if (colorChange == 2){
-	    			b = number;
-	    		}
-	    		ctx.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
-    		}
-    		number = -1;
-    		colorChange = -1;
-    	}
+        this.y += this.speed * 12;
     }
 }
 
-function mouse(event){
-	var x = event.clientX;
-	var y = event.clientY;
+var particles = [];
+var canvas = document.createElement('canvas');
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(canvas);
+var ctx = canvas.getContext("2d");
 
-	if (orix == -1)
-		orix = x;
-	if (oriy == -1)
-		oriy = y;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+setInterval(this.draw, 64);
 
-	points[lastPoint] = x;
-	points[lastPoint+1] = y;
 
-	lastPoint = lastPoint + 2;
+function draw() {
 
-	ctx.beginPath();
-	ctx.arc(x, y, 2, 0, 2 * Math.PI);
-	ctx.fill();
+    ctx.clearRect(0,0,innerWidth, innerHeight);
 
+    if (particles.length < 120) {
+        particles.push(new Particle());
+    }
+
+    for (let i = 0; i < particles.length; i++){
+        particles[i].draw();
+    }
 }
 
-function random(min,max) {
-  var num = Math.floor(Math.random()*(max-min)) + min;
-  return num;
-}
+
